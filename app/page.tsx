664168c0a3d511dacee8a28a448acb94019d4d9d
Althaf0097 <althafs879@gmail.com>
2025-11-27 10:12:21 +0530

    Update homepage mobile grid to 2 columns

--- app/page.tsx
+++ app/page.tsx
@@ -117,7 +117,7 @@ export default async function Home() {
 
         {/* Media Grid */}
         {assets && assets.length > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6">
             {assets.map((asset: any, index: number) => (
               <div
                 key={asset.id}
